9e3bec1ba69f94257d4598bd6767ab6772ad2b69
benseward <ben.seward@googlemail.com>
2022-11-26 22:34:48 +0000

    fix: fix rerendering state bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 import { useStore } from "src/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { initializeStore } = useStore();
-  initializeStore(pageProps.initializeStore);
+
+  useEffect(() => {
+    initializeStore(pageProps.initializeStore);
+  }, []);
 
   return <Component {...pageProps} />;
 }
